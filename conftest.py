2b4870e1aa413070dcce9aa7ce03acb1f340826a
vmarkov <vmarkov@weezlabs.com>
2020-01-24 19:54:12 +0300

    Modified fixture according to the lesson

--- conftest.py
+++ conftest.py
@@ -1,12 +1,24 @@
 import pytest
 from fixture.application import Application
 
+fixture = None
 
-@pytest.fixture(scope="session")
-def app(request):
-    fixture = Application()
-    fixture.session.login(username="admin", password="secret")
 
+@pytest.fixture
+def app():
+    global fixture
+    if fixture is None:
+        fixture = Application()
+        fixture.session.login(username="admin", password="secret")
+    else:
+        if not fixture.is_valid():
+            fixture = Application()
+            fixture.session.login(username="admin", password="secret")
+    return fixture
+
+
+@pytest.fixture(scope="session", autouse=True)
+def stop(request):
     def fin():
         fixture.session.logout()
         fixture.destroy()
