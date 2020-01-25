6c9732dee990fd79b488b3da46c8300f8363ecda
vmarkov <vmarkov@weezlabs.com>
2020-01-25 16:44:42 +0300

    Added checks that group exists to 'modify group' and 'delete group' tests

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,6 +1,9 @@
 from model.group import Group
 
 
+# test from the previous home task
 def test_edit_first_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.edit_first_group(Group(name="Edited name", header="Edited header text", footer="Edited footer text"))
 
