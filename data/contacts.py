4f37e2ad1b12574babccdabb419fbc8f29292f34
vmarkov <vmarkov@weezlabs.com>
2020-02-16 19:41:36 +0300

    Added pre-filled non-random test data for contact creation test

---
+++ data/contacts.py
@@ -0,0 +1,21 @@
+from model.contact import Contact
+
+
+testdata = [
+    Contact(firstname="Tester", middlename="Something", lastname="Matters",
+            photo="picture.jpg", nickname="super nickname", title="QA engineer",
+            company="Google", address="Kremlin", homephone="1111111",
+            mobilephone="2222222", workphone="3333333", fax="4444444",
+            email="test@test.com", email2="test2@test.com", email3="test3@test.com",
+            homepage="google.com", bday="29", bmonth="April", byear="1991", aday="22",
+            amonth="August", ayear="2015", address_2="Moscow", secondaryphone="5555555",
+            notes="Cool guy"),
+    Contact(firstname="Donald", middlename="Ivanovich", lastname="Trump",
+            photo="picture2.jpg", nickname="lover777", title="General manager",
+            company="Yandex", address="Berlin", homephone="123123",
+            mobilephone="234234", workphone="456456", fax="567567",
+            email="check@check.com", email2="check2@check.com", email3="check3@check.com",
+            homepage="yandex.com", bday="12", bmonth="May", byear="1987", aday="13",
+            amonth="September", ayear="2009", address_2="Samara", secondaryphone="90908080",
+            notes="Super engineer")
+]
