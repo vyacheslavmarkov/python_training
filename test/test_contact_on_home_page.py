34c24981fd44da944290ddf15450eebe510e013a
vmarkov <vmarkov@weezlabs.com>
2020-02-25 16:12:01 +0300

    Modified 'test contact on home page' test to compare all the contacts from UI with all the contacts from DB

--- test/test_contact_on_home_page.py
+++ test/test_contact_on_home_page.py
@@ -1,15 +1,9 @@
-from random import randrange
+from model.contact import Contact
 
 
-def test_contact_on_home_page(app):
-    index = randrange(len(app.contact.get_contacts_list()))
-    contact_from_home_page = app.contact.get_contacts_list()[index]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
+def test_contacts_on_home_page(app, db):
+    contacts_from_homepage = app.contact.get_contacts_list()
+    contacts_from_db = db.get_contact_list()
+    contacts_from_db = app.contact.make_contacts_like_on_homepage(contacts_from_db)
+    assert sorted(contacts_from_homepage, key=Contact.id_or_max) == sorted(contacts_from_db, key=Contact.id_or_max)
 
-    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
-    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
-    assert contact_from_home_page.address == contact_from_edit_page.address
-    assert contact_from_home_page.all_phones_from_homepage == \
-           app.contact.merge_phones_like_on_homepage(contact_from_edit_page)
-    assert contact_from_home_page.all_emails_from_homepage == \
-           app.contact.merge_emails_like_on_homepage(contact_from_edit_page)
