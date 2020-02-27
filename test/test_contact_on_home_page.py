e51342d6a3bb6fec75b06212bb3c64415603acf9
vmarkov <vmarkov@weezlabs.com>
2020-02-27 14:57:05 +0300

    Re-implemented 'test contact on home page' test

--- test/test_contact_on_home_page.py
+++ test/test_contact_on_home_page.py
@@ -2,8 +2,13 @@ from model.contact import Contact
 
 
 def test_contacts_on_home_page(app, db):
-    contacts_from_homepage = app.contact.get_contacts_list()
-    contacts_from_db = db.get_contact_list()
-    contacts_from_db = app.contact.make_contacts_like_on_homepage(contacts_from_db)
-    assert sorted(contacts_from_homepage, key=Contact.id_or_max) == sorted(contacts_from_db, key=Contact.id_or_max)
+    contacts_from_home_page = sorted(app.contact.get_contacts_list(), key=Contact.id_or_max)
+    contacts_from_db = sorted(db.get_contact_list(), key=Contact.id_or_max)
 
+    assert len(contacts_from_home_page) == len(contacts_from_db)
+    for index, contact in enumerate(contacts_from_home_page):
+        assert contact.firstname == app.contact.truncate_whitespaces(contacts_from_db[index].firstname)
+        assert contact.lastname == app.contact.truncate_whitespaces(contacts_from_db[index].lastname)
+        assert contact.address == app.contact.truncate_whitespaces(contacts_from_db[index].address)
+        assert contact.all_phones_from_homepage == app.contact.merge_phones_like_on_homepage(contacts_from_db[index])
+        assert contact.all_emails_from_homepage == app.contact.merge_emails_like_on_homepage(contacts_from_db[index])
