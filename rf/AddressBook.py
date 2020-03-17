ccf3e565e2eda3f04b29931b6fad3c70069959cc
vmarkov <vmarkov@weezlabs.com>
2020-03-17 17:42:02 +0300

    Robot Framework: finished example from the lesson

--- rf/AddressBook.py
+++ rf/AddressBook.py
@@ -24,8 +24,19 @@ class AddressBook:
                                password=db_config["password"])
 
     def destroy_fixtures(self):
-        self.dbfixture.destroy()
         self.fixture.destroy()
 
-    def create_group(self, name, header, footer):
-        self.fixture.group.create(Group(name=name, header=header, footer=footer))
+    def new_group(self, name, header, footer):
+        return Group(name=name, header=header, footer=footer)
+
+    def get_group_list(self):
+        return self.dbfixture.get_group_list()
+
+    def create_group(self, group):
+        self.fixture.group.create(group)
+
+    def delete_group(self, group):
+        self.fixture.group.delete_group_by_id(group.id)
+
+    def group_lists_should_be_equal(self, list1, list2):
+        assert sorted(list1, key=Group.id_or_max) == sorted(list2, key=Group.id_or_max)
